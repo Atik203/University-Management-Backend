8b1429d49ccdae2f4ee704ed4b5e3a4ed9241cd8
Atikur Rahaman <atikurrahaman0305@gmail.com>
2024-05-24 00:12:14 +0600

    created user interface

---
+++ src/app/modules/user/user.interface.ts
@@ -0,0 +1,8 @@
+export type TUser = {
+  id: string;
+  password: string;
+  needsPasswordChange: boolean;
+  role: 'admin' | 'student' | 'faculty';
+  status: 'active' | 'blocked';
+  isDeleted: boolean;
+};
