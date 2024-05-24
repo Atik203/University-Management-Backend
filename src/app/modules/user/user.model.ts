78213e792069d3c8d3e257d188a02a19a5431eac
Atikur Rahaman <atikurrahaman0305@gmail.com>
2024-05-24 20:34:59 +0600

    created User model and validation schema

---
+++ src/app/modules/user/user.model.ts
@@ -0,0 +1,39 @@
+import { model, Schema } from 'mongoose';
+import { TUser } from './user.interface';
+
+export const userSchema = new Schema<TUser>(
+  {
+    id: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    needsPasswordChange: {
+      type: Boolean,
+      required: true,
+    },
+    role: {
+      type: String,
+      required: true,
+      enum: ['admin', 'student', 'faculty'],
+    },
+    status: {
+      type: String,
+      enum: ['active', 'blocked'],
+      default: 'active',
+    },
+    isDeleted: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+export const User = model<TUser>('User', userSchema);
