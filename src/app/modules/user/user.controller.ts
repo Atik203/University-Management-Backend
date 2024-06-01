7b7feb7532981a8e2f332aa09c1e65aae7841bf0
Atikur Rahaman <atikurrahaman0305@gmail.com>
2024-06-01 23:40:06 +0600

    created catchAsync function

--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -1,35 +1,23 @@
-import { NextFunction, Request, Response } from 'express';
 import httpStatus from 'http-status';
+import { catchAsync } from '../../utils/catchAsync';
 import { sendResponse } from '../../utils/sendResponse';
 import studentValidationSchema from '../student/student.validation.zod';
 import { userService } from './user.service';
 
-const createStudent = async (
-  req: Request,
-  res: Response,
-  next: NextFunction,
-) => {
-  try {
-    const { password, student } = req.body;
-
-    const zodParseStudent = studentValidationSchema.parse(student);
-
-    const result = await userService.createStudentIntoDB(
-      password,
-      zodParseStudent,
-    );
-
-    sendResponse(res, {
-      success: true,
-      message: 'Student created successfully',
-      statusCode: httpStatus.OK,
-      data: result,
-    });
-  } catch (error) {
-    next(error);
-  }
-};
-
+const createStudent = catchAsync(async (req, res) => {
+  const { password, student } = req.body;
+  const zodParseStudent = studentValidationSchema.parse(student);
+  const result = await userService.createStudentIntoDB(
+    password,
+    zodParseStudent,
+  );
+  sendResponse(res, {
+    success: true,
+    message: 'Student created successfully',
+    statusCode: httpStatus.OK,
+    data: result,
+  });
+});
 export const userController = {
   createStudent,
 };
