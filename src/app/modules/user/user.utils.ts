60034030180e4f59aead4e489f0c3420964f7735
Atikur Rahaman <atikurrahaman0305@gmail.com>
2024-06-07 15:50:02 +0600

    fix student id bug

--- src/app/modules/user/user.utils.ts
+++ src/app/modules/user/user.utils.ts
@@ -12,13 +12,28 @@ const findLastStudentID = async () => {
       createdAt: -1,
     })
     .lean();
-  return lastStudent?.id ? lastStudent.id.substring(6) : undefined;
+  return lastStudent?.id ? lastStudent.id : undefined;
 };
 
 export const generateStudentID = async (payload: TAcademicSemester) => {
-  const currentId =
-    (await findLastStudentID()) || (0).toString().padStart(4, '0');
+  let currentId = (0).toString(); // 0000
+
+  const lastStudentID = await findLastStudentID(); // 2024 01 0001
+  const lastStudentIDYear = lastStudentID?.slice(0, 4); // 2024
+  const lastStudentIDSemesterCode = lastStudentID?.slice(4, 6); // 01
+  const currentSemesterCode = payload.code; // 01
+  const currentYear = payload.year; // 2024
+
+  if (
+    lastStudentID &&
+    lastStudentIDYear === currentYear &&
+    lastStudentIDSemesterCode === currentSemesterCode
+  ) {
+    currentId = lastStudentID?.slice(6); // 0001
+  }
+
   let incrementId = (Number(currentId) + 1).toString().padStart(4, '0');
   incrementId = `${payload.year}${payload.code}${incrementId}`;
+
   return incrementId;
 };
