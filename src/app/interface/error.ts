66942d89b1297c072740215e9826ef83ab7a6d54
Atikur Rahaman <atikurrahaman0305@gmail.com>
2024-06-10 18:09:55 +0600

    Added handleValidationeError funtion  for handling mongoose error

--- src/app/interface/error.ts
+++ src/app/interface/error.ts
@@ -1,4 +1,10 @@
-export type TErrorSource = {
+export type TErrorSources = {
   path: string | number;
   message: string;
 }[];
+
+export type TGenericErrorResponse = {
+  statusCode: number;
+  message: string;
+  errorSources: TErrorSources;
+};
