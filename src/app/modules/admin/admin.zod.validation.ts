9bfc1daba020fb1f48e0eaae303b5f1649f98d4f
Atikur Rahaman <atikurrahaman0305@gmail.com>
2024-07-05 19:02:41 +0600

    update admin zod validation

--- src/app/modules/admin/admin.zod.validation.ts
+++ src/app/modules/admin/admin.zod.validation.ts
@@ -9,7 +9,6 @@ const createUserNameValidationSchema = z.object({
 
 export const createAdminValidationSchema = z.object({
   body: z.object({
-    password: z.string().max(20).optional(),
     admin: z.object({
       designation: z.string(),
       name: createUserNameValidationSchema,
