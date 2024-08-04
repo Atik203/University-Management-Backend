2c26ae6d5ab689af9d4ab383a4f33762344611fd
Atikur Rahaman <atikurrahaman0305@gmail.com>
2024-08-05 01:52:25 +0600

    added super admin

--- src/server.ts
+++ src/server.ts
@@ -3,12 +3,14 @@ import { Server } from 'http';
 import mongoose from 'mongoose';
 import app from './app';
 import config from './app/config';
+import seedSuperAdmin from './app/DB';
 
 let server: Server;
 
 async function main() {
   try {
     await mongoose.connect(config.database_url as string);
+    seedSuperAdmin();
     server = app.listen(config.port, () => {
       console.log(`App listening on port ${config.port}`);
     });
