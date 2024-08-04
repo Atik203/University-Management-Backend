36d27d4084d630112b9a9ed9a5bf02b5883fdfa4
Atikur Rahaman <atikurrahaman0305@gmail.com>
2024-08-05 00:36:37 +0600

    added ulits funtion to calculate grade

---
+++ src/app/modules/enrolledCourse/enrolledCourse.utils.ts
@@ -0,0 +1,24 @@
+export const calculateGradeAndPoint = (totalMarks: number) => {
+  const result = {
+    grade: 'NA',
+    gradePoint: 0.0,
+  };
+  if (totalMarks >= 80 && totalMarks <= 100) {
+    result.grade = 'A';
+    result.gradePoint = 4.0;
+  } else if (totalMarks >= 70) {
+    result.grade = 'B';
+    result.gradePoint = 3.5;
+  } else if (totalMarks >= 60) {
+    result.grade = 'C';
+    result.gradePoint = 3.0;
+  } else if (totalMarks >= 50) {
+    result.grade = 'D';
+    result.gradePoint = 2.0;
+  } else {
+    result.grade = 'F';
+    result.gradePoint = 0.0;
+  }
+
+  return result;
+};
