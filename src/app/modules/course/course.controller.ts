9652bb1a431db0925ad3064fb7736c00eb922b8b
Atikur Rahaman <atikurrahaman0305@gmail.com>
2024-08-05 23:39:56 +0600

    updated course controller

--- src/app/modules/course/course.controller.ts
+++ src/app/modules/course/course.controller.ts
@@ -21,7 +21,8 @@ const getAllCourses = catchAsync(async (req, res) => {
     statusCode: httpStatus.OK,
     success: true,
     message: 'All courses fetched successfully',
-    data: result,
+    meta: result.meta,
+    data: result.result,
   });
 });
 
