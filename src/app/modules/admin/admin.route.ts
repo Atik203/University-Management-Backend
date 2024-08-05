a39405c4a365f9c31e71321e5fef2889a7fa31cf
Atikur Rahaman <atikurrahaman0305@gmail.com>
2024-08-05 17:22:18 +0600

    updated  admin routes

--- src/app/modules/admin/admin.route.ts
+++ src/app/modules/admin/admin.route.ts
@@ -1,25 +1,40 @@
 import express from 'express';
 
+import { auth } from '../../middleware/auth';
 import { parseRequestBodyData } from '../../middleware/parseRequestBodyData';
 import { validateRequest } from '../../middleware/validateRequest';
 import { upload } from '../../utils/sendImageToCloudnary';
+import { USER_ROLE } from '../user/user.constant';
 import { AdminControllers } from './admin.controller';
 import { updateAdminValidationSchema } from './admin.zod.validation';
 
 const router = express.Router();
 
-router.get('/', AdminControllers.getAllAdmins);
+router.get(
+  '/',
+  auth(USER_ROLE.admin, USER_ROLE.superAdmin),
+  AdminControllers.getAllAdmins,
+);
 
-router.get('/:id', AdminControllers.getSingleAdmin);
+router.get(
+  '/:id',
+  auth(USER_ROLE.admin, USER_ROLE.superAdmin),
+  AdminControllers.getSingleAdmin,
+);
 
 router.patch(
   '/:id',
+  auth(USER_ROLE.admin, USER_ROLE.superAdmin),
   upload.single('file'),
   parseRequestBodyData,
   validateRequest(updateAdminValidationSchema),
   AdminControllers.updateAdmin,
 );
 
-router.delete('/:adminId', AdminControllers.deleteAdmin);
+router.delete(
+  '/:adminId',
+  auth(USER_ROLE.admin, USER_ROLE.superAdmin),
+  AdminControllers.deleteAdmin,
+);
 
 export const AdminRoutes = router;
